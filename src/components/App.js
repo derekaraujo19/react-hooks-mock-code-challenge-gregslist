56a2008628d58a2f41d5e7192b8d023902946a71
Derek Araujo <derekaraujo19@gmail.com>
2021-12-03 00:49:42 -0500

    First two core deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
+// DELIVERABLE 1:
+// Create state to house listings
+// useEfect to make GET request to server
+// update state
+// send listings down to ListingsContainer where it will get mapped out to ListingCard
+
+
 function App() {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) => r.json())
+      .then((items) => setListings(items));
+  }, []);
+
+  // console.log(listings);
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} />
     </div>
   );
 }
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+// DELIVERABLE 2:
+// create state for favorite, set initially to false
+// on click, you update state
 
-function ListingCard() {
+// DELIVERABLE 3:
+//
+
+function ListingCard({ img, description, location }) {
+  const [favorite, setFavorite] = useState(false);
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={img} alt={description} />
       </div>
-      <div className="details">
-        {true ? (
+      <div className="details" onClick={() => setFavorite(!favorite)}>
+        {favorite ? (
           <button className="emoji-button favorite active">★</button>
         ) : (
           <button className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
+        <strong>{description}</strong>
+        <span> · {location}</span>
         <button className="emoji-button delete">🗑</button>
       </div>
     </li>
